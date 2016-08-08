0a529cbdb5ed3891731a491e76463062d905b4e2
Roopan S. <roopans@ilink-systems.com>
2016-08-08 13:20:15 +0530

    Latest-Demo

---
+++ Protractor-Net-Demo/BasicTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using FluentAssertions;
+
+namespace Protractor_Net_Demo
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        const string URL = "http://juliemr.github.io/protractor-demo/";
+
+        IWebDriver driver;
+        NgWebDriver ngDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver  = new ChromeDriver();
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
+            ngDriver = new NgWebDriver(driver);
+        }
+
+        [Test]
+        public void Basic_Homepage_ShouldHaveATitle()
+        {
+            ngDriver.Url = URL;
+            var title = ngDriver.Title;
+            title.Should().Be("Super Calculator");
+        }
+
+        [Test]
+        public void Basic_AddOneAndTwo_ShouldBeThree()
+        {
+            ngDriver.Url = URL;
+            var first = ngDriver.FindElement(NgBy.Input("first"));
+            var second = ngDriver.FindElement(NgBy.Input("second"));
+            var goButton = ngDriver.FindElement(By.Id("gobutton"));
+
+            first.SendKeys("1");
+            second.SendKeys("2");
+            goButton.Click();
+            var latestResult = ngDriver.FindElement(NgBy.Binding("latest")).Text;
+
+            latestResult.Should().Be("3");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            ngDriver.Quit();
+        }
+    }
+}
---
+++ Protractor-Net-Demo/PageObjectTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using FluentAssertions;
+using Protractor_Net_Demo.Pages;
+
+namespace Protractor_Net_Demo
+{
+    [TestFixture]
+    public class PageObjectTests
+    {
+        const string URL = "http://juliemr.github.io/protractor-demo/";
+
+        IWebDriver driver;
+        SuperCalculatorPage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
+            page = new SuperCalculatorPage(driver, URL);
+        }
+
+        [Test]
+        public void PageObject_AddOneAndTwo_ShouldBeThree()
+        {
+            page.Add("1", "2");
+            page.LatestResult.Should().Be("3");
+        }
+
+        [Test]
+        public void PageObject_ThreeSubtractOne_ShouldBeTwo()
+        {
+            page.Subtract("3", "1");
+            page.LatestResult.Should().Be("2");
+        }
+
+        [Test]
+        public void PageObject_PerformTwoOperations_ShouldHaveTwoItemsInHistory()
+        {
+            page.Add("1", "1");
+            page.Add("1", "1");
+
+            page.History.Should().HaveCount(2);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
